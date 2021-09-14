35abfcc73ac534caee7070b70e2527ac11968327
Eric Crain <ericcrain77@gmail.com>
2021-09-13 23:06:12 -0400

    adding functions to run webpage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Global Variables //
 // Variable Integers //
-var startTime = 75;
+var time = 75;
 
 var questionNumber = 0;
 
@@ -36,8 +36,6 @@ var answer4 = document.getElementById("answer4");
 
 var answerButton = document.getElementsByClassName("answer-btn");
 
-var answerCheck = document.getElementsByClassName("answer-check");
-
 var finishScreen = document.getElementsByClassName("finish-screen");
 
 var initials = document.getElementsByClassName("initials");
@@ -60,27 +58,27 @@ var questions = [
     {
         q: "Commonly used data types do NOT inlcude:",
         a: ["1. strings", "2. booleans", "3. alerts", "4. Numbers"],
-        correctAnswer: "2"
+        correctAnswer: 2
     },
     {
         q: "The condition in an if/else statment is enclosed within __________.",
         a: ["1. quotes", "2.curly brackets", "3. parenthases", "4. square brackets"],
-        correctAnswer: "2"
+        correctAnswer: 2
     },
     {
         q: "Arrays in JavaScript can be used to store __________.",
         a: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-        correctAnswer: "3"
+        correctAnswer: 3
     },
     {
         q: "String values must be enclosed within __________ when being assigned to variables.",
         a: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthases"],
-        correctAnswer: "2"
+        correctAnswer: 2
     },
     {
         q: "A very useeful tool used during development and debugging for printing content to the debugger is:",
         a: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
-        correctAnswer: "3"
+        correctAnswer: 3
     }
 ];
 // Variables End //
@@ -90,18 +88,38 @@ function startQuiz() {
     document.querySelector(".start-screen").style.visibility = "hidden";
     document.querySelector(".question-screen").style.visibility = "visible";
 
-    question.innerHTML = questions[0].q;
+    // countdownTimer();
+
+    question.innerHTML = questions[questionNumber].q;
    
-    answer1.innerHTML = questions[0].a[0];
-    answer2.innerHTML = questions[0].a[1];
-    answer3.innerHTML = questions[0].a[2];
-    answer4.innerHTML = questions[0].a[3];
+    answer1.innerHTML = questions[questionNumber].a[0];
+    answer2.innerHTML = questions[questionNumber].a[1];
+    answer3.innerHTML = questions[questionNumber].a[2];
+    answer4.innerHTML = questions[questionNumber].a[3];
+}
 
-
+function checkAnswer() {
+    if (answerButton == questions[questionNumber].correctAnswer) {
+        document.querySelector("#correct").style.visibility = "visible";
+    } else {
+        document.querySelector("#wrong").style.visibility = "visible";
+        // timeLeft = time - penalty;
+    }
 }
 
 function nextQuestion() {
+    checkAnswer();
+
     questionNumber++;
 
     
 }
+
+// function checkAnswer() {
+//     if (answerButton !== questions[questionNumber].correctAnswer) {
+//         document.querySelector("#wrong").style.visibility = "visible";
+//     } else {
+//         document.querySelector("#correct").style.visibility = "visible";
+//         // timeLeft = time - penalty;
+//     }
+// }
